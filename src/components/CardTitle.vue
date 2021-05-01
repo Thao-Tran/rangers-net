b0fab4b627051800c8c5b5f7f7214a3c971cf231
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2021-05-01 13:10:40 -0400

    Create CardTitle component
    * Displays a title with a red underline

---
+++ src/components/CardTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card-title>
+    <div class="primary--text">
+      {{ title }}
+      <div class="secondary card-title-underline"></div>
+    </div>
+    <v-spacer/>
+    <slot/>
+  </v-card-title>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+@Component
+export default class CardTitle extends Vue {
+  @Prop(String) title?: string
+}
+</script>
+
+<style lang="scss">
+.card-title-underline {
+  width: 100%;
+  height: 2px;
+}
+</style>
