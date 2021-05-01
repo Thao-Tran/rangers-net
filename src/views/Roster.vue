627445aded58a59e69c70ddf74f9ce027c37df2a
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2021-05-01 13:11:02 -0400

    Add "Player details" card to Roster view
    * Make roster table scrollable
    * Use CardTitle component

--- src/views/Roster.vue
+++ src/views/Roster.vue
@@ -1,37 +1,40 @@
 <template>
-  <div class="roster">
-    <v-card v-if="this.teamName !== undefined" outlined class="roster-card">
-      <v-card-title>
-        <div class="primary--text">
-          {{ teamName }}
-          <div class="secondary roster-card-title-underline"></div>
-        </div>
-        <v-spacer/>
-        <v-btn text color="primary" class="export-btn pa-1">
-          <v-icon>mdi-file-download</v-icon>
-          <div class="mt-1">Export</div>
-        </v-btn>
-      </v-card-title>
-      <v-card-text>
-        <span class="font-weight-bold">Roster status:</span>
-        {{this.rosterStatus}}
+  <div class="roster d-flex">
+    <div v-if="this.teamName !== undefined" class="roster-content" :class="{ 'roster-content-grid': isPlayerSelected, 'd-flex flex-column': !isPlayerSelected }">
+      <v-card v-if="this.teamName !== undefined" outlined class="roster-card d-flex flex-column">
+        <card-title :title="teamName">
+          <v-btn text color="primary" class="export-btn pa-1">
+            <v-icon>mdi-file-download</v-icon>
+            <div class="mt-1">Export</div>
+          </v-btn>
+        </card-title>
+        <v-card-text class="d-flex flex-column">
+          <div>
+            <span class="font-weight-bold">Roster status:</span>
+            {{this.rosterStatus}}
+          </div>
 
-        <v-data-table
-          :headers="headers"
-          :items="players"
-          :items-per-page=20
-          :value="selectedPlayers"
-          hide-default-footer
-          dense
-          single-select
-          class="mt-2"
-        >
-          <template v-slot:[`item.name`]="{ item }">
-            <router-link :to="{ query: { ...$route.query, player: item.id } }" v-text="item.name" class="text-uppercase text-decoration-none font-weight-medium"/>
-          </template>
-        </v-data-table>
-      </v-card-text>
-    </v-card>
+          <v-data-table
+            :headers="headers"
+            :items="players"
+            :items-per-page=20
+            :value="selectedPlayers"
+            fixed-header
+            hide-default-footer
+            dense
+            single-select
+            class="mt-2"
+          >
+            <template v-slot:[`item.name`]="{ item }">
+              <router-link :to="{ query: { ...$route.query, player: item.id } }" v-text="item.name" class="text-uppercase text-decoration-none font-weight-medium"/>
+            </template>
+          </v-data-table>
+        </v-card-text>
+      </v-card>
+      <v-card v-if="isPlayerSelected" outlined class="player-card d-flex flex-column mt-2">
+        <card-title title="Player details"/>
+      </v-card>
+    </div>
     <div v-else class="default-text d-flex align-center justify-center">
       No team selected
     </div>
@@ -39,6 +42,7 @@
 </template>
 
 <script lang="ts">
+import CardTitle from '@/components/CardTitle.vue'
 import { defaultItems } from '@/components/SelectionBox.vue'
 import faker from 'faker'
 import Vue from 'vue'
@@ -55,7 +59,7 @@ interface Player {
   penaltyMinutes: number
 }
 
-@Component
+@Component({ components: { CardTitle } })
 export default class RosterView extends Vue {
   name = 'Roster'
   rosterStatus = 'Approved'
@@ -110,6 +114,10 @@ export default class RosterView extends Vue {
     return this.players.length > 0
   }
 
+  get isPlayerSelected (): boolean {
+    return this.selectedPlayers.length > 0
+  }
+
   get teamName (): string | undefined {
     return defaultItems.team.find(({ value }) => value === this.$route.query.team)?.text
   }
@@ -127,16 +135,31 @@ export default class RosterView extends Vue {
   display: flex;
   padding: 8px;
   width: 100%;
-  height: 100%;
 
-  &-card {
-    display: flex;
-    flex-direction: column;
+  &-content {
     width: 100%;
 
-    &-title-underline {
-      width: 100%;
-      height: 2px;
+    &.roster-content-grid {
+      display: grid;
+      grid-template-rows: 1fr 1fr;
+
+      .v-data-table {
+        overflow: hidden;
+
+        &__wrapper {
+          max-height: 100%;
+          overflow-y: auto;
+        }
+      }
+    }
+  }
+
+  &-card {
+    overflow: hidden;
+    height: 100%;
+
+    .v-card__text {
+      overflow: hidden;
     }
 
     .export-btn {
